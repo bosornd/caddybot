2be335c90e168fe9f670545d0abaf6e4a5a79add
Yong Jin, Cho <drajin.cho@gmail.com>
2023-10-10 02:09:36 +0000

    Adding launches

---
+++ src/caddybot/launch/bringup.py
@@ -0,0 +1,31 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+   dir = os.path.join(get_package_share_directory('caddybot'), 'launch')
+
+   return LaunchDescription([
+      IncludeLaunchDescription(
+         PythonLaunchDescriptionSource([dir, '/fake_gps.py'])
+      ),
+      IncludeLaunchDescription(
+         PythonLaunchDescriptionSource([dir, '/zone.py'])
+      ),
+      IncludeLaunchDescription(
+         PythonLaunchDescriptionSource([dir, '/fake_lidar.py'])
+      ),
+      IncludeLaunchDescription(
+         PythonLaunchDescriptionSource([dir, '/tracker.py'])
+      ),
+      IncludeLaunchDescription(
+         PythonLaunchDescriptionSource([dir, '/sound.py'])
+      ),
+      IncludeLaunchDescription(
+         PythonLaunchDescriptionSource([dir, '/mcu.py'])
+      ),
+   ])
