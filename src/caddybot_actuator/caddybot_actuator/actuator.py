df47b6538a7ff02dc616f712ebb5c244f2dc884a
Yong Jin, Cho <drajin.cho@gmail.com>
2024-01-27 00:35:10 +0000

    Updating caddybot_actuator

--- src/caddybot_actuator/caddybot_actuator/actuator.py
+++ src/caddybot_actuator/caddybot_actuator/actuator.py
@@ -1,11 +1,16 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
 from caddybot_msgs.msg import Velocity
 
 class Actuator(Node):
 
     def __init__(self):
         super().__init__('actuator')
+
+        self.publisher = self.create_publisher(Imu, '/imu', 10)
+        self.publisher = self.create_publisher(Odometry, '/odometry', 10)
 
         self.subscription = self.create_subscription(Velocity, '/velocity', self.callback, 10)
 
